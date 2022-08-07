5663dc9b60889f4ab3567c52c585a26a8955094d
Jade Wright <iamjwright159@gmail.com>
2022-08-07 14:42:04 -0400

    Add basic link support

--- polycule.js
+++ polycule.js
@@ -8,19 +8,36 @@ const graphWidth = 1920, graphHeight = 1080, graphScale = 1;
 	let data = await d3.json('polycule.json');
 
 	let node_data = [];
+	let link_data = [];
 
 	data.nodes.forEach(node => {
 		node_data.push({
-			'name': node.name,
-			'color': node.color
+			id: node.name,
+			name: node.name,
+			color: node.color || '#fff'
 		});
 	});
 
+	data.links.forEach(link => {
+		link_data.push({
+			source: link.a,
+			target: link.b,
+			type: link.type || 'romantic'
+		});
+	})
+
+
+	//---- Graph
 	let svg = d3.select('#graph').append('svg')
 		.classed('svg', true)
 		.attr('viewBox', [0, 0, graphWidth * graphScale, graphHeight * graphScale])
 		.attr('width', graphWidth)
 		.attr('height', graphHeight);
+	
+	let links = svg.selectAll('.link')
+		.data(link_data)
+		.join('line')
+		.classed('link', true);
 	
 	let nodes = svg.selectAll('.node')
 		.data(node_data)
@@ -44,8 +61,14 @@ const graphWidth = 1920, graphHeight = 1080, graphScale = 1;
 	let simulation = d3.forceSimulation(node_data)
 		.force('charge', d3.forceManyBody().strength(-50))
 		.force('center', d3.forceCenter(graphWidth * graphScale / 2, graphHeight * graphScale / 2))
-		//.force('link', d3.forceLink(links).id(node => node.id).distance(20))
+		.force('link', d3.forceLink(link_data).id(node => node.id).distance(20))
 		.on('tick', () => {
+			links
+				.attr('x1', link => link.source.x)
+				.attr('y1', link => link.source.y)
+				.attr('x2', link => link.target.x)
+				.attr('y2', link => link.target.y);
+
 			nodes
 				.attr('transform', node => `translate(${node.x}, ${node.y})`)
 				.select('circle')
@@ -88,5 +111,10 @@ const graphWidth = 1920, graphHeight = 1080, graphScale = 1;
 	
 	
 	//---- Final attribute/data calls, after links are made
+	nodes
+		.each(node => node.weight = links.filter(link => link.source === node || link.target === node).size());
 
+	simulation.force('link')
+		.strength(link => 0.2 / Math.min(link.source.weight, link.target.weight))
+		.distance(50);
 })();
